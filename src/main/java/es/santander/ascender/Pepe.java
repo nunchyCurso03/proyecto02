dbd5437d23debf4b1cf117a4b4c492e4a337c22b
curso03 <you@example.com>
2024-11-04 13:35:33 +0100

    Separando preparar, ejecutar y verificar en los test

---
+++ src/main/java/es/santander/ascender/Pepe.java
@@ -0,0 +1,10 @@
+package es.santander.ascender;
+
+public class Pepe {
+    public int sumar(int a, int b){
+        //TODO cambiar el valor de retorno
+        //throw new UnsupportedOperationException("Aún no")
+        return a + b;
+
+    }
+}
---
+++ src/test/java/es/santander/ascender/PepeTest.java
@@ -0,0 +1,17 @@
+package es.santander.ascender;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class PepeTest {
+    @Test
+    public void testSumar(){
+        //preparo
+        Pepe sut= new Pepe();
+
+        //Ejecuto
+        int resultado =sut.sumar(4, 3);
+
+        //Verifico
+        assertTrue(resultado==7);
+    }
+}
